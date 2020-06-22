d5ced49a10d72120002a10d9f2423ca6e3336697
JovanNikolic <jovan.nikolic.dev@outlook.com>
2020-06-22 22:22:04 +0200

    logo-img, SVG progress bars

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
   	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 1)";
     document.getElementById("nav-wrapper").style.padding = "16px 16px";
-    document.getElementById("logo").style.fontSize = "32px";
+    document.getElementById("logo").style.transform = "scale(.8)";
+
+
   } else {
   	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 0.5)";
     document.getElementById("nav-wrapper").style.padding = "32px 16px";
-    document.getElementById("logo").style.fontSize = "48px";
+    document.getElementById("logo").style.transform = "scale(1)";
+
+
   }
 }
