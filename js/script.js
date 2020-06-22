2b0d25c4dd7511b1c07543e467d16cec330110c2
JovanNikolic <jovan.nikolic.dev@outlook.com>
2020-06-22 17:16:11 +0200

    about-lead, nav

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 1)";
     document.getElementById("nav-wrapper").style.padding = "16px 16px";
     document.getElementById("logo").style.fontSize = "32px";
   } else {
+  	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 0.5)";
     document.getElementById("nav-wrapper").style.padding = "32px 16px";
     document.getElementById("logo").style.fontSize = "48px";
   }
