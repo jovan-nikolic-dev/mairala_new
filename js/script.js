e79af044d4865149c0e8b71f5ef3f63904d85f1d
JovanNikolic <jovan.nikolic.dev@outlook.com>
2020-06-23 18:58:56 +0200

    grid, nav edits

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
   	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 1)";
-    document.getElementById("nav-wrapper").style.padding = "16px 16px";
+    document.getElementById("nav-wrapper").style.padding = "16px 0px";
     document.getElementById("logo").style.transform = "scale(.8)";
 
 
   } else {
   	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 0.5)";
-    document.getElementById("nav-wrapper").style.padding = "32px 16px";
+    document.getElementById("nav-wrapper").style.padding = "32px 0px";
     document.getElementById("logo").style.transform = "scale(1)";
 
 
