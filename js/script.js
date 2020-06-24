ed972650912860c7f18c9a9d2dc3702265efcab8
JovanNikolic <jovan.nikolic.dev@outlook.com>
2020-06-24 22:50:43 +0200

    nav underline

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
-window.onscroll = function() {scrollFunction()};
+let nav = document.getElementById("nav");
+let logo = document.getElementById("logo");
+let navWrapper = document.getElementById("nav-wrapper");
+
+window.onscroll = function() {
+	scrollFunction();
+}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-  	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 1)";
-    document.getElementById("nav-wrapper").style.padding = "16px 0px";
-    document.getElementById("logo").style.transform = "scale(.8)";
-
-
-  } else {
-  	document.getElementById("nav").style.backgroundColor = "rgba(48, 48, 48, 0.5)";
-    document.getElementById("nav-wrapper").style.padding = "32px 0px";
-    document.getElementById("logo").style.transform = "scale(1)";
-
-
-  }
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+		navWrapper.style.minHeight = "60px";
+		logo.style.height = "27px";
+		nav.style.backgroundColor = "rgba(48, 48, 48, 1)";
+	} else {
+		navWrapper.style.minHeight = "95px";
+		logo.style.height = "38px";
+		nav.style.backgroundColor = "rgba(48, 48, 48, 0.5)";		
+	}
 }
+
+
+
+let navLink = document.getElementsByClassName("nav-link");
+console.log(navLink);
+
+
+console.log(navLink.length);
+
+for (let i = 0; i < navLink.length; i++) {
+	let underline = document.createElement("DIV");
+	underline.style.height = "3px";
+	underline.style.width = "100%";
+	underline.style.position = "absolute";
+	underline.style.bottom = "0";
+	underline.style.left = "0";
+	underline.style.backgroundColor = "#7b6c63";
+	underline.style.opacity = "0";
+	underline.style.transition = ".4s ease-in-out";
+	navLink[i].appendChild(underline);
+
+	navLink[i].onmouseover = function() {
+		underline.style.opacity = "1";
+	}
+
+	navLink[i].onmouseout = function() {
+		underline.style.opacity = "0";
+	}
+}
+
+
